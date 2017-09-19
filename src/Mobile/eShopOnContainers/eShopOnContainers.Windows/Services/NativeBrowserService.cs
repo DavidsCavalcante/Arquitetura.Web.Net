721449bbcc3094ce4af5c5ca58ceb3872000229f
David Britch <davidbritch@users.noreply.github.com>
2017-09-19 13:50:36 +0100

    Added native browser to UWP project.

---
+++ src/Mobile/eShopOnContainers/eShopOnContainers.Windows/Services/NativeBrowserService.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.Web;
+using Xamarin.Forms;
+using eShopOnContainers.Windows.Services;
+using eShopOnContainers.Core.Services.Dependency;
+using eShopOnContainers.Core.Models.Browser;
+using eShopOnContainers.Windows.Controls;
+
+[assembly:Dependency(typeof(NativeBrowserService))]
+namespace eShopOnContainers.Windows.Services
+{
+    public class NativeBrowserService : INativeBrowserService
+    {
+        async Task<BrowserResult> INativeBrowserService.LaunchBrowserAsync(string url)
+        {
+            var browser = new WebAuthenticationBrokerBrowser();
+            var redirectUri = WebAuthenticationBroker.GetCurrentApplicationCallbackUri().AbsoluteUri;
+
+            //url = url.Replace(WebUtility.UrlEncode(Constants.RedirectUri), WebUtility.UrlEncode(redirectUri));
+            //Constants.RedirectIro = redirectUri;
+
+            return await browser.InvokeAsync(url);
+        }
+    }
+}
